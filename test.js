086668c5e408ea9d3780c1c0c03d1ccb6bdaa4ee
Jan-Strum <janusbased@gmail.com>
2019-05-06 16:42:30 -0500

    Reorganized getDate tests so that hours, seconds, and decimal offsets are their own unit tests.

--- test.js
+++ test.js
@@ -16,7 +16,7 @@ const {
 } = require('./helpers')
 
 describe('getLocationData', () => {
-  it('should return an object with name, locationKey, and GmtOffset properties given a valid input', async () => {
+  it('should return an object with name, locationKey, and GmtOffset properties given a valid input.', async () => {
     const locationData = await getLocationData('Berlin')
     expect(locationData).to.be.an('object')
     expect(locationData).to.have.property('name')
@@ -26,25 +26,30 @@ describe('getLocationData', () => {
 })
 
 describe('getDate', () => {
-  it('should return a time as a Date instance', () => {
+  let chicagoDate, newYorkDate, mumbaiDate
+  beforeEach(() => {
+    chicagoDate = getDate(-5)
+    newYorkDate = getDate(-4)
+    mumbaiDate = getDate(5.5)
+  })
+
+  it('should return a time as a Date instance.', () => {
     const time = getDate(-6)
     expect(time).to.be.an.instanceOf(Date)
   })
-  it('should return the correct time provided a GMT offset that is a whole number', () => {  // We will compare hours, minutes, and period (AM and PM), but not seconds, so as to account for computation time.
-    const chicagoDate = getDate(-5),
-          newYorkDate = getDate(-4)
-
+  it('should return a time with the correct hour.', () => {  // We will check hours, minutes, and period (AM and PM), but not seconds, so as to account for computation time.
     expect(getHour(chicagoDate)).to.equal(getHourWithGmtOffset(GmtTime, -5))
-    expect(getMinute(chicagoDate)).to.equal(getMinute(GmtTime))
-    expect(checkPeriod(chicagoDate, -5)).to.be.true
-
     expect(getHour(newYorkDate)).to.equal(getHourWithGmtOffset(GmtTime, -4))
+  })
+  it('should return a time with the correct minute.', () => {
+    expect(getMinute(chicagoDate)).to.equal(getMinute(GmtTime))
     expect(getMinute(newYorkDate)).to.equal(getMinute(GmtTime))
+  })
+  it('should return a time with the correct period.', () => {
+    expect(checkPeriod(chicagoDate, -5)).to.be.true
     expect(checkPeriod(newYorkDate, -4)).to.be.true
   })
-  it('should return the correct time provided a GMT offset that is a decimal', () => {
-    const mumbaiDate = getDate(5.5)
-
+  it('should return the correct time provided a GMT offset that is a decimal.', () => {
     if (getMinute(mumbaiDate) >= 30) {
       expect(getHour(mumbaiDate)).to.equal(getHourWithGmtOffset(GmtTime, 5))
     } else {
@@ -53,11 +58,10 @@ describe('getDate', () => {
     expect(getMinute(mumbaiDate) % 60).to.equal((getMinute(GmtTime) + 30) % 60)
     expect(checkPeriod(mumbaiDate, 5.5)).to.be.true
   })
-
 })
 
 describe('getCurrentConditions', () => {
-  it('should return a string containing a temperature and a unit', async () => {
+  it('should return a string containing a temperature and a unit.', async () => {
     const conditions = await getCurrentConditions(348308)
     const temperature = conditions.split(' ')[0]
     const unit = conditions.split(' ')[1]
@@ -68,14 +72,14 @@ describe('getCurrentConditions', () => {
 })
 
 describe('buildMessage', () => {
-  it('should return a string', async () => {
+  it('should return a string.', async () => {
     const message = await buildMessage('Oaxaca')
     expect(message).to.be.a('string')
   })
 })
 
 describe('printmessages', () => {
-  it('should return an array of strings', async () => {
+  it('should return an array of strings.', async () => {
     const messages = await printmessages(["No Man's Land", 'Chicago', 10028, 'Mumbai'])
     expect(messages).to.be.an('array')
     messages.forEach(message => expect(message).to.be.a('string'))
